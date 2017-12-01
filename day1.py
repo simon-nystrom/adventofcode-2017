3f092ac07fb03541f99cfbcd6b1f93d44fbc1fd9
Simon Nystrom <simon.nystrom@accedo.tv>
2017-12-01 09:10:33 +0100

    Clean up

--- day1.py
+++ day1.py
@@ -2,21 +2,9 @@ file = open("input-1.txt")
 sequence = file.readlines()[0]
 file.close()
 
-def solve1(sequence): 
+def solve(sequence, offset):
     result = 0
     i = 0
-    while i < len(sequence):
-        if sequence[i] == sequence[(i + 1) % len(sequence)]:
-            result += int(sequence[i])
-        i += 1
-
-    return result
-
-
-def solve2(sequence):
-    result = 0
-    i = 0
-    offset = len(sequence) // 2
     while i < len(sequence):
         if sequence[i] == sequence[(i + offset) % len(sequence)]:
             result += int(sequence[i])
@@ -24,7 +12,8 @@ def solve2(sequence):
 
     return result
 
-print(solve1(sequence))
-print(solve2(sequence))
+
+print(solve(sequence, 1))
+print(solve(sequence, len(sequence) // 2))
 
 
