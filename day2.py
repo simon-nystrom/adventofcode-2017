6505b2018de2dc1e0d79feffff7ac6ad0a5fa612
Simon Nyström <simon.nystrom90@gmail.com>
2017-12-02 09:03:43 +0100

    Clean up

--- day2.py
+++ day2.py
@@ -2,25 +2,10 @@ file = open("input-2.txt")
 chart = [x.strip() for x in file.readlines()]
 file.close()
 
+def row_result_max_min(row):
+    return max(row) - min (row)
 
-def solve1(chart):
-    result = 0
-    for l in chart:
-        row = [int(x) for x in l.split('\t')]
-        result += max(row) - min(row)
-    return result
-
-
-def solve2(chart):
-    result = 0
-    for l in chart:
-        row = [int(x) for x in l.split('\t')]
-        result += row_result(row)
-
-    return result
-
-
-def row_result(row):
+def row_result_div(row):
     for i, x in enumerate(row):
         for j, y in enumerate(row):
             if x % y == 0 and i != j:
@@ -28,6 +13,12 @@ def row_result(row):
             elif y % x == 0 and i != j:
                 return  y // x
 
+def solve(chart, row_op):
+    result = 0
+    for l in chart:
+        row = [int(x) for x in l.split('\t')]
+        result += row_op(row)
+    return result
 
-print(solve1(chart))
-print(solve2(chart))
+print(solve(chart, row_result_max_min))
+print(solve(chart, row_result_div))
