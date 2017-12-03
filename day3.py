11e7108550928007a7998c50cc7a37e3ae416f94
Simon Nyström <simon.nystrom@accedo.tv>
2017-12-03 14:20:26 +0100

    Update day3.py

--- day3.py
+++ day3.py
@@ -84,11 +84,9 @@ def solve2(puzzle_input):
     grid = [[0 for x in range(layers)] for y in range(layers)]
     row = col = layers // 2  # Start in the middle
     grid[row][col] = 1
-    i = 0
     spiral_gen = spiral_loop(grid, row, col, 2)
     while current_value <= puzzle_input:
         current_value = next(spiral_gen)
-        i += 1
     return current_value
 
 
