99a4e6c53f42197306497819f5205d719c002a68
Simon Nyström <simon.nystrom90@gmail.com>
2017-12-14 12:20:49 +0100

    Day 13 solved

--- day13.py
+++ day13.py
@@ -1,26 +1,42 @@
 file = open("input-13.txt")
-puzzle_input = [x.strip().replace(":", "").split(' ') for x in file.readlines()]
+puzzle_input = [list(map(int, x.strip().replace(":", "").split(' '))) for x in file.readlines()]
 file.close()
 
 def solve(puzzle_input):
 
-    current_position = 0
-    scanners = {}
+    severity = 0
     scanner_pos = 0
-    for i, scanner in enumerate(puzzle_input):
-        scanners[scanner[0]] = int(scanner[1]) - 1
-        loop = i // scanners["0"]
-        scanner_pos = i % scanners["0"]
+    for i, depth in puzzle_input:
+        loop = i // (depth - 1)
+        scanner_pos = i % (depth - 1)
         if loop > 0 and loop % 2 != 0:
-            scanner_pos = scanners["0"] - scanner_pos
+            scanner_pos = depth - 1 - scanner_pos
+        if scanner_pos == 0:
+            severity += i * depth
 
-            
-        print(scanner_pos)
-        
+    return severity
 
+def solve1(puzzle_input):
+    scanner_pos = 0
+    delay = 0
+    max_reach = 0
+    detected = True
+    while (detected):
+        for i, depth in puzzle_input:
+            detected = False
+            loop = (i + delay) // (depth - 1)
+            scanner_pos = (i + delay) % (depth - 1)
+            if loop > 0 and loop % 2 != 0:
+                scanner_pos = depth - 1 - scanner_pos
+            if scanner_pos == 0:
+                detected = True
+                break
+        delay += 1
 
-    return "end"
+    # print(max_reach)
+    return delay - 1
 
 
 print(solve(puzzle_input))
+print(solve1(puzzle_input))
 
