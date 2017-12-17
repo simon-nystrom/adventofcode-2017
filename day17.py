efc3a4e3b73c1b81cd872b46493e2e751a4109f6
Simon Nyström <simon.nystrom90@gmail.com>
2017-12-17 21:44:28 +0100

    Part 2 solved

--- day17.py
+++ day17.py
@@ -1,13 +1,36 @@
 spinlock_step = 328
 
-circular_buffer = [0]
 
-current_size = 1
-current_position = 0
-for x in range(1, 50000000):
-    pos = (spinlock_step + current_position) % len(circular_buffer)
-    # print(pos)
-    current_position = pos +  1
-    circular_buffer.insert(current_position, x)
+def solve1(spinlock_step):
+    circular_buffer = [0]
+    current_position = 0
+    for x in range(1, 2018):
+        pos = (spinlock_step + current_position) % len(circular_buffer)
+        current_position = pos + 1
+        circular_buffer.insert(current_position, x)
 
-print(circular_buffer[(current_position + 1) % len(circular_buffer)])
+    return circular_buffer[(current_position + 1) % len(circular_buffer)]
+
+
+def solve2(spinlock_step):
+    current_position_of_zero = 0
+    current_position = 0
+    after_zero = 0
+    current_length = 1
+    for x in range(1, 50000001):
+
+        pos = (spinlock_step + current_position) % current_length
+        
+        if pos == current_position_of_zero:
+            after_zero = x
+        elif pos < current_position_of_zero:
+            current_position_of_zero += 1
+
+        current_position = pos + 1
+        current_length += 1
+
+    return after_zero
+
+
+print(solve1(spinlock_step))
+print(solve2(spinlock_step))
